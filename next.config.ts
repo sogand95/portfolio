99e0403fa5eaa1ce358a7f4943423bcdfd823be3
unknown <sogand.aghahoseini@gmail.com>
2025-02-01 07:25:48 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      // cspell:disable-next-line
+      "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      // cspell:disable-next-line
+      "picsum.photos",
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
